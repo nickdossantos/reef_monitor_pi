3b73c6af8200787d6e6f9fcaf8d7505eee79631b
Nick Dos Santos <nickdos32@gmail.com>
2019-03-18 23:04:53 +0000

    added basic form on index page to authenticate pi

--- webapp.py
+++ webapp.py
@@ -1,89 +1,107 @@
 #Author Nick Dos Santos
 from flask import Flask
 from flask import request
+from flask import render_template
 from flask_admin import Admin
-from flask_admin.contrib.peewee import ModelView
 from flask import jsonify
-import RPi.GPIO as GPIO
+from dotenv import load_dotenv
 from temperature import TempSensor
-import jwt
+import json
+import requests
 
+import RPi.GPIO as GPIO
+
+import jwt
+import os
 import model
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nickjds' 
 
-data = model.Data()
+@app.route("/index")
+def index():
+	return render_template("index.html")
 
-admin = Admin(app, name='Reef Admin Page', template_mode='bootstrap3', url='/')
-admin.add_view(ModelView(model.User))
-admin.add_view(ModelView(model.Device))
+@app.route("/verify_pin", methods=['POST'])
+def verify_pin():
+	#post pin to web app verifying the user.
+	#get response if user is already verified return message, 
+	#if user was veryified send sucess.
+	#if there was an error print the error.
+	pin_number = request.form['pin_number']
+	URL = "http://1992cfd8.ngrok.io/api/verify_pin_number/?pin_number=" + pin_number
+	# defining a params dict for the parameters to be sent to the API 
+	PARAMS = {'pin_number':pin_number} 
+  
+	# sending get request and saving the response as response object 
+	r = requests.post(URL)
+	response = json.loads(r.text)
+	print response
+	print "this is r"
+	return str(response['status'])
+@app.route("/api/device_status/all", methods=['POST'])
+def get_status_all():
+        try:
+		decoded = jwt.decode(request.args.get('token'), 'iliketurtles', algorithms=['HS256'])
+                if decoded['auth_token'] == os.getenv("AUTH_TOKEN"):
+                	GPIO.setmode(GPIO.BCM)
+                	response = []
+                	for device in decoded['devices']:
+                        	GPIO.setup(device['pin_number'], GPIO.OUT) # GPIO Assign mode
+                        	device_status = GPIO.input(device['pin_number'])
+                        	if device_status == 1: 
+                                	device_status = True
+                        	else: 
+                                	device_status = False
+                        	device_obj = {'device': device['id'], 'status': device_status}
+                        	response.append(device_obj)
+		return jsonify(response)
+        except IOError as e:
+                return e.to_json
 
-@app.route("/api/device_status", methods=['POST'])
-def get_status():
-	try:
-        	GPIO.setmode(GPIO.BCM)
-		response = []
-		for device in data.get_devices():
-      			GPIO.setup(device.pin, GPIO.OUT) # GPIO Assign mode
-			device_status = GPIO.input(device.pin)
-			if device_status == 1: 
+def get_status(devices):
+        try:
+		GPIO.setmode(GPIO.BCM)
+        	response = []
+                for device in devices:
+			GPIO.setup(device['pin_number'], GPIO.OUT) # GPIO Assign mode
+                        device_status = GPIO.input(device['pin_number'])
+                        if device_status == 1: 
 				device_status = True
 			else: 
 				device_status = False
-			device_obj = {'device':device.identifier, 'status':device_status}
-			response.append(device_obj)
-	except:
-		print("there was an error")
-	return jsonify(response)
-
-@app.route("/api/create_temperature_reading", methods=['POST'])
-def create_termperature_reading():
-	user = data.find_user(request.args.get('auth_token'))
-	if user:
-		temp = TempSensor()
-		sensor = temp.sensor()
-		post = temp.post_reading(sensor, user.api_endpoint + "readings")
-		return "it posted"
-	else: 
-		return "user could not be found"
-
+			device_obj = {'device': device['id'], 'status': device_status}
+                        response.append(device_obj)
+                return jsonify(response)
+        except IOError as e:
+                return e.to_json
 
 @app.route("/api/get_temperature_reading", methods=['GET'])
 def get_termperature_reading():
-        token = request.args.get('token')
-        decoded = jwt.decode(token, 'iliketurtles', algorithms=['HS256'])
-        auth_token = decoded['auth_token']
-        identifier = decoded['hash_id']
-	pin_number = decoded['pin_number']
-	user = data.find_user(auth_token)
-	
-        user = data.find_user(auth_token)
-        if user:
+        load_dotenv()
+        decoded = jwt.decode(request.args.get('token'), 'iliketurtles', algorithms=['HS256'])
+	if decoded['auth_token'] == os.getenv("AUTH_TOKEN"):
                 temp = TempSensor()
                 sensor = temp.sensor()
                 return jsonify(temp.get_reading())
         else: 
-                return "user could not be found"
+                return "Unauthorized"
 
 @app.route("/api/turn_on_device", methods=["POST"])
 def turn_on_device():
 	try:
-		token = request.args.get('token')
-       	 	decoded = jwt.decode(token, 'iliketurtles', algorithms=['HS256'])
-		auth_token = decoded['auth_token']
-		identifier = decoded['hash_id']
-		if token and identifier:
-			user = data.find_user(auth_token)
-                	device = data.find_device(identifier)
+		load_dotenv()
+       	 	decoded = jwt.decode(request.args.get('token'), 'iliketurtles', algorithms=['HS256'])
+		if decoded['auth_token'] == os.getenv("AUTH_TOKEN"):
 	                GPIO.setmode(GPIO.BCM)
         	
                 	GPIO.setwarnings(False)
 
-                	GPIO.setup(device.pin, GPIO.OUT) # GPIO Assign mode
+                	GPIO.setup(decoded['pin_number'], GPIO.OUT) # GPIO Assign mode
 
-               		GPIO.output(device.pin, GPIO.HIGH) # on  
+               		GPIO.output(decoded['pin_number'], GPIO.HIGH) # on  
 
-               		return get_status()
+               		return get_status(decoded['devices'])
 
 		else:
 			return "Could not find a user or device with information provided"
@@ -94,22 +112,18 @@ def turn_on_device():
 @app.route("/api/turn_off_device", methods=["POST"])
 def turn_off_device():
 	try:
-        	token = request.args.get('token')
-        	decoded = jwt.decode(token, 'iliketurtles', algorithms=['HS256'])
-        	auth_token = decoded['auth_token']
-        	identifier = decoded['hash_id']
-        	if token and identifier:
-                	user = data.find_user(auth_token)
-                	device = data.find_device(identifier)
+		load_dotenv()
+        	decoded = jwt.decode(request.args.get('token'), 'iliketurtles', algorithms=['HS256'])
+        	if decoded['auth_token'] == os.getenv("AUTH_TOKEN"):
                 	GPIO.setmode(GPIO.BCM)
         
                 	GPIO.setwarnings(False)
 
-                	GPIO.setup(device.pin, GPIO.OUT) # GPIO Assign mode
+                	GPIO.setup(decoded['pin_number'], GPIO.OUT) # GPIO Assign mode
 
-                	GPIO.output(device.pin, GPIO.LOW) # off  
+                	GPIO.output(decoded['pin_number'], GPIO.LOW) # off  
 
-                	return get_status()
+                	return get_status(decoded['devices'])
 
         	else:
                 	return "Could not find a user or device with information provided"
@@ -117,3 +131,6 @@ def turn_off_device():
         	return "I am done"
 	except IOError as e:
 		return e.to_json
+
+if __name__ == "__webapp__":
+	app.run()
